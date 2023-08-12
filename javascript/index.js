105c15c5c877100d9592d071c29029f5eeb19e42
LizzieMac209 <Emacfarlane05@gmail.com>
2023-08-11 20:39:23 -0700

    Inserted javascript to read real time date and times of cities on display list

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,41 @@
+function updateSanFranciscoTime() {
+  let sanFranciscoElement = document.querySelector("#san-francisco");
+  let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
+  let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
+  let sanFranciscoTime = moment().tz("America/Los_Angeles");
 
+  sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("MMMM Do YYYY");
+  sanFranciscoTimeElement.innerHTML = sanFranciscoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateSanFranciscoTime();
+setInterval(updateSanFranciscoTime, 1000);
+
+function updateBarcelonaTime() {
+  let barcelonaElement = document.querySelector("#barcelona");
+  let barcelonaDateElement = barcelonaElement.querySelector(".date");
+  let barcelonaTimeElement = barcelonaElement.querySelector(".time");
+  let barcelonaTime = moment().tz("Europe/Madrid");
+
+  barcelonaDateElement.innerHTML = barcelonaTime.format("MMMM Do YYYY");
+  barcelonaTimeElement.innerHTML = barcelonaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateBarcelonaTime();
+setInterval(updateBarcelonaTime, 1000);
+
+function updateSingaporeTime() {
+  let singaporeElement = document.querySelector("#singapore");
+  let singaporeDateElement = singaporeElement.querySelector(".date");
+  let singaporeTimeElement = singaporeElement.querySelector(".time");
+  let singaporeTime = moment().tz("Singapore");
+
+  singaporeDateElement.innerHTML = singaporeTime.format("MMMM Do YYYY");
+  singaporeTimeElement.innerHTML = singaporeTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateSingaporeTime();
+setInterval(updateSingaporeTime, 1000);
